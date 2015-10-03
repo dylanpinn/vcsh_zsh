ef16684d8bc2e56043fb6506543e5e3add738589
Dylan Pinn <dylan.pinn@gmail.com>
2015-10-03 10:16:12 +1000

    Add rbenv, nvm, pyenv

--- .zsh/path.zsh
+++ .zsh/path.zsh
@@ -2,3 +2,15 @@
 
 # Antibody (OSX)
 source "$(brew --prefix)/share/antibody.zsh"
+
+# rbenv
+if which rbenv > /dev/null;
+  then eval "$(rbenv init -)";
+fi
+
+# nvm
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+
+# pyenv
+if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
