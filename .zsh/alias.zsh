4316ac257867904fd5541acdf586a936ac5dacf5
Dylan Pinn <dylan.pinn@gmail.com>
2015-10-06 15:42:37 +1100

    Add alias !! to redo last command with sudo

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -2,6 +2,9 @@
 __reload_dotfiles() {
   PATH="$(command -p getconf PATH):/usr/local/bin"
   . ~/.zshrc
-  cd .
+  cd ... || exit
 }
 alias reload!='__reload_dotfiles'
+
+# Redo last command with sudo
+alias !!='sudo $(fc -ln -1)'
